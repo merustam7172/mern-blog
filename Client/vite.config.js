c8ec43b0a90817cc5c3734de8f748f4c2009ce6c
merustam7172 <merustam7172@gmail.com>
2024-11-09 14:52:49 +0530

    Connect Backend to Frontend & added Functionality to the sign-up page

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+
+// use for fetching data when api is from backend
 export default defineConfig({
+  server : {
+    proxy : {
+      '/api' : {
+        target : 'http://localhost:3000',
+        secure : false,
+      }
+    }
+  },
   plugins: [react()],
 })
