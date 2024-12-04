81512f67bb9e4873d0acd0de3b983d3c5c396697
merustam7172 <merustam7172@gmail.com>
2024-12-04 19:25:25 +0530

    creating search filter api post route(getPosts)

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -56,3 +56,53 @@ export const uploadPostImage = async (req, res, next) => {
     next(error);
   }
 };
+
+
+// get posts
+
+export const getPosts = async(req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId : req.query.userId}),
+      ...(req.query.category && { userId : req.query.category}),
+      ...(req.query.slug && { userId : req.query.slug}),
+      ...(req.query.postId && { userId : req.query.postId}),
+      ...(req.query.searchTerm && { 
+        $or : [
+          {title : {$regex : req.query.searchTerm, $options : 'i'}},
+
+          {content : {$regex : req.query.searchTerm, $options : 'i'}}
+        ]
+      }),
+    }).sort({ updatedAt : sortDirection}).skip(startIndex).limit(limit);
+
+
+    // count the total number of posts
+    const totalPosts = await Post.countDocuments();
+
+    // count total post in last month
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt : {$gte : oneMonthAgo}
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts
+    });
+
+  } catch (error) {
+    next(error);
+  }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create, uploadPostImage } from "../controllers/post.controller.js";
+import { create, getPosts, uploadPostImage } from "../controllers/post.controller.js";
 import upload from "../utils/multer.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
 router.post('/upload-postImage',verifyToken, upload.single('PostImage'), uploadPostImage);
+router.get('/getposts', getPosts);
 export default router
