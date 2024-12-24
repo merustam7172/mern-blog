fa4b35045023aae5dbfc8eee6b214c59cb76b244
merustam7172 <merustam7172@gmail.com>
2024-12-24 12:38:02 +0530

    add recent articles to the post page

--- Client/src/pages/PostPage.jsx
+++ Client/src/pages/PostPage.jsx
@@ -2,11 +2,13 @@ import { Spinner, Button } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -31,6 +33,22 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPost = async() => {
+        const res = await fetch(`/api/post/getPosts?limit=3`);
+        const data = await res.json();
+        if(res.ok){
+          setRecentPosts(data.posts);
+        }
+      }
+      fetchRecentPost();
+    } catch (error) {
+      console.log(error.message)
+    }
+   
+  },[])
 
   if (loading)
     return (
@@ -60,13 +78,22 @@ const PostPage = () => {
 
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className="italic">{post && (post.content.length / 1000).toFixed(0)}</span>
+        <span className="italic">{post && (post.content.length / 1000).toFixed(0)} mins read</span>
       </div>
 
       <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html : post && post.content}}>
       </div>
 
       <CommentSection postId={post._id}/>
+
+      {/* recent articles */}
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </div>
   );
 };
