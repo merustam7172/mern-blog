3504aac7e884b2aeb41be5736a839f2ade8d3108
merustam7172 <merustam7172@gmail.com>
2024-12-25 12:21:15 +0530

    added home page ui and its functionlity

--- Client/src/components/PostCard.jsx
+++ Client/src/components/PostCard.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const PostCard = ({ post }) => {
   return (
-    <div>
+    
       <div className="group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all">
         <Link to={`/post/${post.slug}`}>
           <img
             src={post.image}
             alt="post cover"
-            className="h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20"
+            className="h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20 object-center"
           />
         </Link>
         <div className="p-3 flex flex-col gap-2">
@@ -23,7 +23,6 @@ const PostCard = ({ post }) => {
           </Link>
         </div>
       </div>
-    </div>
   );
 };
 
